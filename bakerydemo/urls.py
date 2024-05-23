dd46268f02ef087eac635bf43d721635c75c0d29
yaweiwang <xdjm562@qq.com>
2024-05-23 17:10:53 +0800

    replace fav icon

--- bakerydemo/urls.py
+++ bakerydemo/urls.py
@@ -40,7 +40,7 @@ if settings.DEBUG:
     urlpatterns += [
         path(
             "favicon.ico",
-            RedirectView.as_view(url=settings.STATIC_URL + "img/bread-favicon.ico"),
+            RedirectView.as_view(url=settings.STATIC_URL + "img/piek-favicon.ico"),
         )
     ]
 
