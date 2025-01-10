22e65836a467e9375aef86996d6dde63c9843036
yaweiwang <xdjm562@qq.com>
2025-01-10 16:47:29 +0800

    fix piek clien building

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # 收集所有app（包括第三方）的静态文件到settings中配置的静态目录中
 # 一般只有第一次需要，或者app静态文件发生变化时需要
-# python manage.py collectstatic
+python manage.py collectstatic
 # 构建镜像
 docker build -t registry.cn-hangzhou.aliyuncs.com/myelin2021/piek-client .
 # 登陆阿里云的ARC
